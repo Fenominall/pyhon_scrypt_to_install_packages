e4709cf966ff1c3cdf11c8ebdffb7becab131917
Fenominall <68444378+Fenominall@users.noreply.github.com>
2021-03-21 09:34:14 +0200

    Update automate_packes.py

--- automate_packes.py
+++ automate_packes.py
@@ -3,5 +3,5 @@ import subprocess
 
 
 # implement pipenv as subprocess
-subprocess.check_call([sys.executable, '-m', 'pipenv', 'install', 'django', 'pillow', 'bcrypt', 'pylint-django'])
+subprocess.check_call([sys.executable, '-m', 'pipenv', 'install', 'django', 'pillow', 'bcrypt', 'pylint-django', 'django-filter'])
 subprocess.check_output([sys.executable, 'm', 'pipenv', 'shell'])
